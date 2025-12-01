689e15f20a5803c0ba2886edc4549c6163af4962
Mariaregue-spec <mariareguecurso@gmail.com>
2025-12-01 18:59:17 +0100

    2nd sheep addition test

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 function countSheep (list){
 let sheep = 0;
 for (let index = 0; index < list.length; index++){  
-    if (list[index] === true){
-        sheep ++;
-    }
+if (list[index] === true){
+sheep ++;
+}
 }
 if(sheep > 0) {
-    console.log("There are " + sheep + "sheep in total");
+console.log("There are " + sheep + "sheep in total");
 }
 if (sheep === 0) {
     console.log("Ups!!! Wolfs eaten Sheeps");
 }
+}
 countSheep([ true, true, true, false, true, true, true, true, true, false, true, false, true, false, false, true, true, true, true, true, false, false, true, true ]);
+countSheep( [ false, false, false ])
